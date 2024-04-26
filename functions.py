9188ec418359ae90f9739850e49c3190c7f216f3
David Fuge <dave-fuge@web.de>
2024-04-26 17:30:28 +0200

    habe die functionen und variablen umbenannt

--- functions.py
+++ functions.py
@@ -12,18 +12,18 @@ def getRandomQuestionWithAnswers():
             cursor = connection.cursor(dictionary=True)
             # Select a random question
             cursor.execute("SELECT id, difficulty, question FROM questions ORDER BY RAND() LIMIT 1")
-            random_question = cursor.fetchone()
-            if random_question:
+            randomQuestion = cursor.fetchone()
+            if randomQuestion:
                 # Fetch all answers for the selected question
-                cursor.execute("SELECT answer, is_correct FROM answers WHERE question_id = %s", (random_question['id'],))
+                cursor.execute("SELECT answer, is_correct FROM answers WHERE question_id = %s", (randomQuestion['id'],))
                 answers = cursor.fetchall()
                 # Construct the array with difficulty, question, and answers
-                question_with_answers = {
-                    "difficulty": random_question['difficulty'],
-                    "question": random_question['question'],
+                questionWithAnswers = {
+                    "difficulty": randomQuestion['difficulty'],
+                    "question": randomQuestion['question'],
                     "answers": answers
                 }
-                return question_with_answers
+                return questionWithAnswers
                 
             else:
                 logging.info("No questions found.")
@@ -40,17 +40,16 @@ def getRandomQuestionWithAnswers():
 
 if __name__ == "__main__":
     
-    random_question_with_answers = getRandomQuestionWithAnswers()
-    if random_question_with_answers:
-        print(random_question_with_answers)
+    randomQuestionWithAnswers = getRandomQuestionWithAnswers()
+    if randomQuestionWithAnswers:
+        print(randomQuestionWithAnswers)
         logging.info("Random Question:")
-        logging.info("Difficulty: %s", random_question_with_answers['difficulty'])
-        logging.info("Question: %s", random_question_with_answers['question'])
+        logging.info("Difficulty: %s", randomQuestionWithAnswers['difficulty'])
+        logging.info("Question: %s", randomQuestionWithAnswers['question'])
         logging.info("Answers:")
         
-        for answer in random_question_with_answers['answers']:
+        for answer in randomQuestionWithAnswers['answers']:
             logging.info("- %s (Correct: %s)", answer['answer'], answer['is_correct'])
             
     else:
         logging.info("No questions found.")
-    
